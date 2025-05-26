a02db107a0451ef5ae0dd5480ebae4af7c5cde26
周旭斌 <zhouxubin@qimao.com>
2025-05-26 13:50:02 +0800

    [Modify] 修改输出

--- mc_math/main.py
+++ mc_math/main.py
@@ -31,5 +31,36 @@ def subtract(a: int, b: int) -> int:
     """
     return a - b
 
+@mcp.tool()
+async def hot_news() -> str:
+    """
+    搜索今天的热点新闻，把所有的新闻整理输出
+
+    Returns:
+        搜索结果的总结
+    """
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            'http://v.juhe.cn/toutiao/index',
+            params={
+                "type": "top",
+                "key": "0f6d7a923525308e50a295401b8680b7",
+                "page": 1,
+                "page_size": 30
+            }
+        )
+        
+        # res_data = []
+        # for data in response.json()['result']['data']:
+        #     for message in choice['message']['tool_calls']:
+        #         search_results = message.get('search_result')
+        #         if not search_results:
+        #             continue
+        #         for result in search_results:
+        #             res_data.append(result['content'])
+
+        return response.text
+
 if __name__ == "__main__":
     mcp.run()
